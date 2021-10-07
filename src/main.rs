cb26413ef03effc1e7e4e96841a6ff72deb72b42
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-07 04:49:57 +0200

    added actix-web sample with get and put endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+use handlers::{hello, echo};
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind("127.0.0.1:8081")?
+    .run()
+    .await
 }
