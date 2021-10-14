d5125d163e74a236d5523c7116fb8ac936b543c0
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-14 02:53:08 +0200

    Started to implement diesel

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,11 @@
 use actix_web::{get, post, HttpResponse, Responder};
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn get_note_by_id() -> impl Responder {
+    HttpResponse::Ok().body("note")
 }
 
 #[post("/")]
-pub async fn echo(req_body: String) -> impl Responder {
+pub async fn create_note(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
+use diesel::{PgConnection, r2d2::ConnectionManager};
+use diesel::r2d2;
+use actix_web::{App, HttpServer, web};
+use handlers::{get_note_by_id, create_note};
+
 mod handlers;
-use handlers::{hello, echo};
-use actix_web::{App, HttpServer};
+mod models;
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    HttpServer::new(move || {
+        App::new().data(pool.clone())
+        .service(
+            web::scope("/api/v1")
+                .service(get_note_by_id)
+                .service(create_note)
+        )
     })
-    .bind("127.0.0.1:8081")?
+    .bind("localhost:8080")?
     .run()
     .await
 }
