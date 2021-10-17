8fe2ca442d1945c63d0f146d7e6a9995167c09d8
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-18 00:39:38 +0200

    changed port

--- src/discovery.rs
+++ src/discovery.rs
@@ -4,7 +4,7 @@ pub fn init_eureka(
 ) -> EurekaClient {
     let mut config = BaseConfig::default();
     config.instance.ip_addr = "localhost".to_string();
-    config.instance.port = Some(PortData::new(8082, true));
+    config.instance.port = Some(PortData::new(8081, true));
     let eureka = EurekaClient::new(config);
     eureka.start();
     eureka
